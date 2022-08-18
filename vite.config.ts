da7440f5f4273f0587b9daf0b0764a33901d5fb5
xiaoshunshi <song159769@163.com>
2022-08-18 11:17:01 +0800

    feat: ceshi

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import eslintPlugin from 'vite-plugin-eslint'
+
 import path from 'path'
 const resolve = (dir: string) => path.join(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      cache: false
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve('src')
